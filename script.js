0b6c5a8ac85d5918a62695eb28a7adbf34c56898
Levon <levon.hakopyan@google.com>
2014-07-02 09:22:12 +0400

    move javascript code to script file

---
+++ script.js
@@ -0,0 +1,41 @@
+var homeUrl = "http://lev-hak.url.ph";
+var loginUrl = homeUrl + "/wp-login.php"
+var redirectUrl = loginUrl + "?action=wordpress_social_authenticate&provider=Google&redirect_to=" + loginUrl;
+/*
+var check = function(status) {
+	if(status == 500) {
+		window.location.assign(homeUrl);
+	}
+	else {
+		window.location.assign(redirectUrl);
+	}
+};
+
+$(function() {
+	$.ajax({
+		type: "GET",
+		url: redirectUrl,
+		complete: function(xhr, textStatus) {
+			//window.location.assign(homeUrl);
+			//check(xhr.status);
+		}
+	});
+});*/
+	
+var inAppBrowser = null;
+
+var initInAppBrowser = function() {
+	return window.open(homeUrl + '/mobile.php', '_self', 'location=yes');
+};
+
+var initMenuKeyDown = function() {
+	inAppBrowser.document.addEventListener("menuKeyDown", function(){ 
+		alert("menu key is pressed");
+	}, false);
+};
+
+document.addEventListener("deviceready", function(){ 
+	inAppBrowser = initInAppBrowser();
+	initMenuKeyDown();
+}, false); 
+
